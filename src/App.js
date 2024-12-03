e65e03262093d1277764576087c17ee8f335d5cc
Rasim <gera9008@gmail.com>
2024-12-03 22:15:28 +0200

    Create App.js

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./styles.css"; // Імпорт CSS стилів
+
+const App = () => {
+  return (
+    <div className="app-container">
+      <h1>Hello, World!</h1>
+    </div>
+  );
+};
+
+export default App;
